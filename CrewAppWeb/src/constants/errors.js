f17f7117efbf1e4bfdae792bb6263332d7cd763f
KaIRaTBeK <45318061+kairatbek-d@users.noreply.github.com>
2023-04-28 20:16:01 +0000

    Define application-wide error message constants

---
+++ CrewAppWeb/src/constants/errors.js
@@ -0,0 +1,5 @@
+export const ERRORS = {
+    LOGIN_FAIL_ERROR: 'Something went wrong...',
+    NETWORK_OR_SYNTAX_ERROR: 'Something went wrong.. Check your internet connection or contact us: umutbek@mail.com.',
+    SOMETHING_WENT_WRONG: 'Something went wrong...'
+  }
