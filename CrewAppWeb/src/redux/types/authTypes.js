d959831a48e9c57e49e1f2647c5ac24a629f2f29
KaIRaTBeK <45318061+kairatbek-d@users.noreply.github.com>
2023-05-31 19:53:32 +0000

    Define action type constants for Redux

---
+++ CrewAppWeb/src/redux/types/authTypes.js
@@ -0,0 +1,8 @@
+export const USER_LOADING = 'USER_LOADING'
+export const USER_SUCCESS = 'USER_SUCCESS'
+export const USER_FAIL = 'USER_FAIL'
+export const LOGIN_LOADING = 'LOGIN_LOADING'
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
+export const LOGIN_FAIL = 'LOGIN_FAIL'
+export const LOGOUT = 'LOGOUT'
+export const CLEAR_ERROR = 'CLEAR_ERROR'
